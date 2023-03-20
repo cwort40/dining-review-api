6c223bb20629b8a9f920ede5d2134afcc9e55a6e
Christian Worthen <110493480+cwort40@users.noreply.github.com>
2023-03-20 11:20:09 -0500

    initial commit

---
+++ src/test/java/worthen/projects/diningreviewapi/DiningReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package worthen.projects.diningreviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
